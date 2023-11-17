f20b3fd7bedb89562bdb2e13c94fd7c2b7d09cf9
nattalvv <nataliastec33@gmail.com>
2023-11-17 16:43:04 +0100

    lab2 solution

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,50 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public: 
+        ResourceManager() 
+        {
+        	ob = new Resource();
+		}
+        
+        double get()  
+        {
+        	return ob ->get();
+		}
+        
+        ResourceManager(const ResourceManager& i)  
+        {
+        	ob = new Resource();
+        	*ob = *(i.ob);	
+		}
+        
+    	ResourceManager& operator=(const ResourceManager& i)  
+    	{
+    		*ob = *i.ob;
+    		return *this;  
+		}
+		
+		ResourceManager(ResourceManager&& i)  
+		{
+			ob = (i.ob);
+			i.ob = nullptr;  
+		}
+		
+		ResourceManager& operator=(ResourceManager&& i) 
+    	{
+    		delete ob;
+    		ob = i.ob;
+    		i.ob = nullptr;
+    		return *this;
+		}
+		
+		~ResourceManager()
+		{
+			delete ob;
+		}
+		
+    private: 
+        Resource* ob; 
 };
